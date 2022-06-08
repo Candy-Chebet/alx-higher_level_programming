f9d99561774d4628e24021d8ccc5dba4283c500a
Candy-Chebet <chebetcandychebet@gmail.com>
2022-06-08 16:18:32 -0700

    second commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
         return None
-    max_value = max(a_dictionary.values())
-    for k, v in a_dictionary.items():
-        if max_value == v:
-            return 
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key 
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    return sum(list(set(my_list)))
+def common_elements(set_1, set_2):
+    return(set_1.intersection(set_2))
