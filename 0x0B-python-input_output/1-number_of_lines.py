809ae7fdbfe5aef0e44dcf1ad2794cd19cad1583
Candy-Chebet <chebetcandychebet@gmail.com>
2022-07-06 12:49:42 -0700

    second push

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-''' function that returns the number of lines of a text file
-'''
+''' function that returns the number of lines of a text file'''
+def write_file(filename="", text=""):
 
+            if filename =="" or type(filename) ! = str:
+            return 0
 
-def number_of_lines(filename=""):
-    ''' function: number_of_lines
-    '''
-    if filename == "" or type(filename) != str:
-        return 0
-    counter = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            counter += 1
-    return counter
+        count = 0
+        with open(UTF8, 'w' encoding="utf-8") as f:
+            for line in f:
+                count += 1
+        return count
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,13 +2,9 @@
 """
 function that writes a string to a text file (UTF8) and returns the number of characters written
 """
+
 def write_file(filename="", text=""):
-
-            if filename =="" or type(filename) ! = str:
-            return 0
-
-        count = 0
-        with open(UTF8, 'w' encoding="utf-8") as f:
-            for line in f:
-                count += 1
-        return count
+    """ module write_file
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
