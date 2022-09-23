3d0c47ed34ba1f934578a5d567ba5cba7c2cb4fd
Candy-Chebet <chebetcandychebet@gmail.com>
2022-09-22 18:37:02 -0700

    python network basics

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
-""" A bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-
-*The size must be in bytes.
-*You have to use 'curl'
-"""
-
+#!/usr/bin/python3
+""" A bash script that takes in a URL, sends a request to that URL,and displays the size of the body of the response.*The size must be in bytes.*You have to use 'curl'."""
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
 
